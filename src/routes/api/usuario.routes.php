92c847ef37fe863b807f28360beb090547095fac
Jorge R <jorge.rosado7@outlook.com>
2026-04-10 20:49:39 +0200

    Se añade registro a ruta usuario y se refactoriza el código

--- src/routes/api/usuario.routes.php
+++ src/routes/api/usuario.routes.php
@@ -1,17 +1,9 @@
 <?php
-
 require_once __DIR__ . "/../../config/database.php";
-require_once __DIR__ . "/../../controllers/UsuarioController.php";
-
-
-header("Content-Type: application/json; charset=UTF-8");
-
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+require_once __DIR__ . "/../../controllers/usuarioController.php";
 
 $database = new Database();
 $db = $database->getConnection();
-
 $controller = new UsuarioController($db);
 
 $method = $_SERVER['REQUEST_METHOD'];
@@ -20,21 +12,30 @@ function getInputData() {
     return json_decode(file_get_contents("php://input"));
 }
 
-if ($method === "POST") {
-
-    $data = getInputData();
-
-    if (!$data) {
+    if ($method === "POST") {
+        $data = getInputData();
+        if (!$data) {
+            http_response_code(400);
+            echo json_encode(["success" => false, "message" => "JSON inválido"]);
+            exit;
+        }
+        if (isset($data->email, $data->password) && !isset($data->nombre)) {
+            $controller->login($data);
+        }
+        else if (isset($data->nombre, $data->email, $data->password)) {
+            $controller->register($data);
+        }
+        else {
+            http_response_code(400);
+            echo json_encode([
+                "success" => false,
+                "message" => "Datos incompletos"
+            ]);
+        }
+    } else {
+        http_response_code(405);
         echo json_encode([
             "success" => false,
-            "message" => "Body vacío"
+            "message" => "Método no permitido"
         ]);
-        exit;
     }
-
-    $controller->login($data);
-    exit;
-}
-
-http_response_code(405);
-echo json_encode(["message" => "Método no permitido"]);
