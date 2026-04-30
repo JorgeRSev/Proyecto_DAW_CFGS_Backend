569ff442363bd1344fe3107a4ed53bf685e9d8c9
Jorge R <jorge.rosado7@outlook.com>
2026-04-30 02:28:39 +0200

    cabecera ngrok

--- src/routes/api.routes.php
+++ src/routes/api.routes.php
@@ -1,7 +1,8 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, ngrok-skip-browser-warning");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("ngrok-skip-browser-warning: true");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
